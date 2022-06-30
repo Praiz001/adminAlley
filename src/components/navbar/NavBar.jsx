7bc90cc968da8458d9d397794ea8293dbbbd1621
O.K Praise <praizbrave@yahoo.com>
2022-06-30 15:38:32 +0100

    Navbar Ui

--- src/components/navbar/NavBar.jsx
+++ src/components/navbar/NavBar.jsx
@@ -1,13 +1,56 @@
-import './navbar.scss'
+import "./navbar.scss";
+
+import SearchOutlinedIcon from "@mui/icons-material/SearchOutlined";
+import LanguageOutlinedIcon from "@mui/icons-material/LanguageOutlined";
+import DarkModeOutlinedIcon from "@mui/icons-material/DarkModeOutlined";
+import FullscreenExitOutlinedIcon from "@mui/icons-material/FullscreenExitOutlined";
+import NotificationsNoneOutlinedIcon from "@mui/icons-material/NotificationsNoneOutlined";
+import ChatBubbleOutlineOutlinedIcon from "@mui/icons-material/ChatBubbleOutlineOutlined";
+import ListOutlinedIcon from "@mui/icons-material/ListOutlined";
 
 const NavBar = () => {
   return (
-    <div className='navBar'>
+    <div className="navBar">
       <div className="wrapper">
-        Nav Bar
+        <div className="search">
+          <input id="search" type="text" placeholder="Search..." />
+          <label htmlFor="search">
+            <SearchOutlinedIcon />
+          </label>
+        </div>
+        <div className="items">
+          <div className="item">
+            <LanguageOutlinedIcon className="icon" />
+            English
+          </div>
+          <div className="item">
+            <DarkModeOutlinedIcon />
+          </div>
+          <div className="item">
+            <FullscreenExitOutlinedIcon />
+          </div>
+          <div className="item">
+            <NotificationsNoneOutlinedIcon />
+            <div className="counter">5</div>
+          </div>
+          <div className="item">
+            <ChatBubbleOutlineOutlinedIcon />
+            <div className="counter">2</div>
+          </div>
+          <div className="item">
+            <ListOutlinedIcon />
+          </div>
+          <div className="item">
+            <img
+              src="https://images.pexels.com/photos/941693/pexels-photo-941693.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500"
+              alt=""
+              className="avatar"
+            />
+          </div>
+        </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default NavBar
+export default NavBar;

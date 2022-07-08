1f247024beb7a1ad6834bcdabcaf79f561ca456e
O.K Praise <praizbrave@yahoo.com>
2022-07-08 02:32:46 +0100

    homepage ui

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -3,14 +3,35 @@ import "./home.scss";
 import NavBar from "../../components/navbar/NavBar";
 
 import SideBar from "../../components/sidebar/SideBar";
+import Widget from "../../components/widget/Widget"
+import Featured from "../../components/featured/Featured";
+import Chart from "../../components/chart/Chart";
+
+import Table from "../../components/table/Table";
 
 const Home = () => {
+  
   return (
     <div className="home">
       <SideBar />
       <div className="homecontainer">
         <NavBar />
-        Home Container
+        <div className="widgets">
+          <Widget type="user" />
+          <Widget type="orders" />
+          <Widget type="earnings" />
+          <Widget type="balance" />
+        </div>
+
+        <div className="charts">
+          <Featured />
+          <Chart />
+        </div>
+
+        <div className="listContainer">
+          <div className="listTitle">Latest Transactions</div>
+          <Table /> 
+        </div>
       </div>
     </div>
   );
